0102d99d0b588c6282bd5f7bdced05e91632a85f
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-04-24 12:48:00 +0300

    Update test.py

--- KDE_Based_Algorithm/test.py
+++ KDE_Based_Algorithm/test.py
@@ -1,8 +1,8 @@
 from kde_based_algorithm import KDE_Based_Algorithm
 
-main_dir = '../content/drive/MyDrive/data/s1/'
+main_dir = '../content/drive/MyDrive/s9/'
 x=0
 print('Enter desired recall:')
 x = input()
-sg = KDE_Based_Algorithm(budget=500000, qPairs = 100, delimiter='\t',  sourceFilePath=main_dir + 'sourceSample.tsv', targetFilePath=main_dir + 'targetSample.tsv', users_input=float(x))
+sg = KDE_Based_Algorithm(budget = 5679576, qPairs = 2362497, delimiter='\t',  sourceFilePath=main_dir + 'sourceSample.tsv', targetFilePath=main_dir + 'targetSample.tsv', users_input=float(x))
 sg.applyProcessing()
