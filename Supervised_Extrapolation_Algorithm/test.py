50ccd8b6bbdac1c0e95224deb54d0d542c46c0f6
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-04-24 11:45:10 +0300

    Update test.py

--- Supervised_Extrapolation_Algorithm/test.py
+++ Supervised_Extrapolation_Algorithm/test.py
@@ -1,8 +1,8 @@
 from extrapolation import Extrapolation
 
-main_dir = '../content/drive/MyDrive/s2/'
+main_dir = '../content/drive/MyDrive/s9/'
 x=0
 print('Enter desired recall:')
 x = input()
-sg = Extrapolation(budget=500000, qPairs = 100, delimiter='\t',  sourceFilePath=main_dir + 'sourceSample.tsv', targetFilePath=main_dir + 'targetSample.tsv', users_input=float(x))
+sg = Extrapolation(budget=5679576, qPairs = 2362497, delimiter='\t',  sourceFilePath=main_dir + 'sourceSample.tsv', targetFilePath=main_dir + 'targetSample.tsv', users_input=float(x))
 sg.applyProcessing()
