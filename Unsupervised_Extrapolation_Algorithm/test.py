beb263c1fac0ee413f7d321ee0ac83191cca6580
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-04-24 12:22:20 +0300

    Update test.py

--- Unsupervised_Extrapolation_Algorithm/test.py
+++ Unsupervised_Extrapolation_Algorithm/test.py
@@ -1,8 +1,8 @@
 from extrapolation import Extrapolation
 
-main_dir = '../content/drive/MyDrive/data/s1/'
+main_dir = '../content/drive/MyDrive/s9/'
 x=0
 print('Enter desired recall:')
 x = input()
-alg = Extrapolation(budget=500000, qPairs = 100, delimiter='\t',  sourceFilePath=main_dir + 'sourceSample.tsv', targetFilePath=main_dir + 'targetSample.tsv', wScheme = 'CF',  users_input=float(x))
+alg = Extrapolation(budget=5679576, qPairs = 2362497, delimiter='\t',  sourceFilePath=main_dir + 'sourceSample.tsv', targetFilePath=main_dir + 'targetSample.tsv', wScheme = 'JS_APPROX',  users_input=float(x))
 alg.applyProcessing()
