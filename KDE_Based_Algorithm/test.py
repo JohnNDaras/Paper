c89a23dd4c5512deea96a8ad6f7f14d1d0967429
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-08-07 15:48:53 +0300

    Delete KDE_Based_Algorithm/test.py

--- KDE_Based_Algorithm/test.py
+++
@@ -1,8 +0,0 @@
-from kde_based_algorithm import KDE_Based_Algorithm
-
-main_dir = '../content/drive/MyDrive/s9/'
-x=0
-print('Enter desired recall:')
-x = input()
-sg = KDE_Based_Algorithm(budget = 5679576, qPairs = 2362497, delimiter='\t',  sourceFilePath=main_dir + 'sourceSample.tsv', targetFilePath=main_dir + 'targetSample.tsv', users_input=float(x))
-sg.applyProcessing()
