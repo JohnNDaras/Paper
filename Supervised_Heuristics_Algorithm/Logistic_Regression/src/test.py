9323aa3e3994cc317ae28fe283ee2a2d544d290d
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-08-07 16:13:28 +0300

    Add files via upload

---
+++ Supervised_Heuristics_Algorithm/Logistic_Regression/src/test.py
@@ -0,0 +1,25 @@
+import os
+import weka.core.jvm as jvm
+from kde_based_algorithm import KDE_Based_Algorithm
+
+
+def setup_environment():
+    os.environ['JAVA_HOME'] = "/usr/lib/jvm/java-8-openjdk-amd64"
+    os.environ["CLASSPATH"] = "weka/weka-3-8-5/weka.jar"
+    jvm.start()
+
+def main():
+    main_dir = "/home/njdaras/Downloads/Geosfiles/data/"
+    recall = input('Enter desired recall: ')
+    
+    sg = KDE_Based_Algorithm(budget=5679576, qPairs=2362497, delimiter='\t',
+                             sourceFilePath=os.path.join(main_dir, 'regions_gr.csv'),
+                             targetFilePath=os.path.join(main_dir, 'wildlife_sanctuaries.csv'),
+                             target_recall=float(recall))
+    sg.applyProcessing()
+
+if __name__ == "__main__":
+    setup_environment()
+    main()
+
+
