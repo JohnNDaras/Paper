8c943858696b4a808e1185738f9fbb865476d8b3
JohnNDaras <117290033+JohnNDaras@users.noreply.github.com>
2024-08-07 15:37:09 +0300

    Delete Supervised_Extrapolation_Algorithm/Logistic_Regression/src/test.py

--- Supervised_Extrapolation_Algorithm/Logistic_Regression/src/test.py
+++
@@ -1,5 +0,0 @@
-from heuristics_algorithm import Heuristics_Algorithm
-
-main_dir = '../content/drive/MyDrive/s9/'
-sg = Heuristics_Algorithm(budget=5679576, qPairs = 2362497, delimiter='\t',  sourceFilePath=main_dir + 'sourceSample.tsv', targetFilePath=main_dir + 'targetSample.tsv', HeuristicCondition = "Dynamic_Qualifying_Distance_Threshold", ConditionLimit = 10000 , DynamicFactor = 0.5, ViolationLimit = 0)
-sg.applyProcessing()
